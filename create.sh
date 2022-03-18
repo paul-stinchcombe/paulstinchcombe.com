b8673989e7a55d336b9aade6f9f9e904ea496385
Paul Stinchcombe <paul@dragonsoftcommunications.com>
2022-03-18 10:26:29 +0800

    creator

---
+++ create.sh
@@ -0,0 +1,7 @@
+echo "# paulstinchcombe.com" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/paul-stinchcombe/paulstinchcombe.com.git
+git push -u origin main
